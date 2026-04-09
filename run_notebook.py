b62ec1c0c5f15e06b32b824a36a69bbfd4a8c0d5
zeinabbh <zeinabbh12@gmail.com>
2026-04-10 00:21:27 +0200

    added compositional analysis (sccoda) for datasets 1 and 4

---
+++ run_notebook.py
@@ -0,0 +1,15 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+filename = "/rds/general/user/ztb25/home/PBMC_datasets/6/dataset6_preprocessing.ipynb"
+outname = "/rds/general/user/ztb25/home/PBMC_datasets/6/dataset6_preprocessing_executed.ipynb"
+workdir = "/rds/general/user/ztb25/home/PBMC_datasets/6/"
+
+with open(filename, encoding="utf-8") as f:
+    nb = nbformat.read(f, as_version=4)
+
+ep = ExecutePreprocessor(timeout=-1, kernel_name="python3")
+ep.preprocess(nb, {"metadata": {"path": workdir}})
+
+with open(outname, "w", encoding="utf-8") as f:
+    nbformat.write(nb, f)
