2c2060199c71c73e0a2b064988b80286fcbc541d
zeinabbh <zeinabbh12@gmail.com>
2026-04-21 09:12:00 +0100

    tried scanpro, added comparison to reference ranges

--- run_notebook.py
+++ run_notebook.py
@@ -8,7 +8,7 @@ workdir = "/rds/general/user/ztb25/home/PBMC_datasets/6/"
 with open(filename, encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=4)
 
-ep = ExecutePreprocessor(timeout=-1, kernel_name="python3")
+ep = ExecutePreprocessor(timeout=-1, kernel_name="m10")
 ep.preprocess(nb, {"metadata": {"path": workdir}})
 
 with open(outname, "w", encoding="utf-8") as f:
